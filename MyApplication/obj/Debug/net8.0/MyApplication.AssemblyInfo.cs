cb5eaca7b1290feb33d48b2f8d19372402d4cb5b
Owen Knutson <123429162+Dr-Waffle75@users.noreply.github.com>
2025-04-14 10:55:54 -0500

    restarting on making line graph
    i hate blazor

--- MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60866a31dbf8ebdf2c4914df3321575c2081c5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502b36d387e8eb8c8c83c3ab89eedcead1286239")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

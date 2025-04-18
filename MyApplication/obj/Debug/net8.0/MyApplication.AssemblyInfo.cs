fe14ae5cb765430e1aac95f6e3c17cab00007137
Owen Knutson <123429162+Dr-Waffle75@users.noreply.github.com>
2025-04-18 10:51:27 -0500

    fixed the x axis for real this time

--- MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08a8c877bc8b6dc15642efbeeb9b882bdddf4c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9367a102054f97a031a90cacff7b5aeaaf26f9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

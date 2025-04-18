c9367a102054f97a031a90cacff7b5aeaaf26f9f
Owen Knutson <123429162+Dr-Waffle75@users.noreply.github.com>
2025-04-18 09:48:14 -0500

    fixed commits to github

--- MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc8544069c6dfec134822be88d91ddc51ece457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08a8c877bc8b6dc15642efbeeb9b882bdddf4c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
